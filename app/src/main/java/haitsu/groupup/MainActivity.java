78fa0b7335e27eef179b780767ddfeff5a2d02a4
Heights94 <mohammed_omar94@hotmail.co.uk>
2017-06-08 17:52:14 +0100

    Revert "Remade project with Navigation Drawer"
    This reverts commit b50184a5ca24d278c935ff661c262a2d952dd5e3.

--- app/src/main/java/haitsu/groupup/MainActivity.java
+++ app/src/main/java/haitsu/groupup/MainActivity.java
@@ -2,20 +2,13 @@ package haitsu.groupup;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -24,19 +17,23 @@ import android.widget.Toast;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,
-        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+import static android.R.attr.id;
+
+public class MainActivity extends AppCompatActivity implements
+          GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     private GoogleApiClient mGoogleApiClient;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -66,36 +63,17 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        //helloWorld();//Writes message to Firebase
 
         // Assign fields
-        mSignOutButton = (Button) findViewById(R.id.sign_out);
-        //mDeleteAccountButton = (Button) findViewById(R.id.delete_account);
-       // mSubmitButton = (Button) findViewById(R.id.submit_group);
+        mSignOutButton = (Button) findViewById(R.id.sign_out_Google);
+        mDeleteAccountButton = (Button) findViewById(R.id.delete_account);
+        mSubmitButton = (Button) findViewById(R.id.submit_group);
 
         // Set click listeners
         mSignOutButton.setOnClickListener(this);
-       // mDeleteAccountButton.setOnClickListener(this);
-       // mSubmitButton.setOnClickListener(this);
+        mDeleteAccountButton.setOnClickListener(this);
+        mSubmitButton.setOnClickListener(this);
 
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -119,8 +97,9 @@ public class MainActivity extends AppCompatActivity
             writeNewUser(mFirebaseUser.getUid(), mFirebaseUser.getDisplayName(), mFirebaseUser.getEmail());
 
         }
-    }
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
 
     private void writeNewUser(final String userId, String name, String email) {
         final User user = new User(name, email);
@@ -140,7 +119,6 @@ public class MainActivity extends AppCompatActivity
         //Use push() for auto generated id node.
     }
 
-/*
     private void submitNewGroup() {
         RadioButton group = (RadioButton) findViewById(R.id.football_radioButton);
         EditText text = (EditText) findViewById(R.id.editText);
@@ -151,7 +129,6 @@ public class MainActivity extends AppCompatActivity
         databaseRef.child("Group").child(group.getText().toString()).child("Members").child(mFirebaseUser.getUid()).setValue(true);//Add users
         //Use push() for auto generated id node.
     }
-*/
 
     private void signOut(){
         FirebaseAuth.getInstance().signOut();//Sign out of Firebase.
@@ -183,15 +160,15 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.sign_out:
+            case R.id.sign_out_Google:
                 signOut();
                 break;
-           // case R.id.delete_account:
-           //     deleteAccount();
-           //     break;
-        //    case R.id.submit_group:
-       //         submitNewGroup();
-       //         break;
+            case R.id.delete_account:
+                deleteAccount();
+                break;
+            case R.id.submit_group:
+                submitNewGroup();
+                break;
         }
     }
 
@@ -206,64 +183,5 @@ public class MainActivity extends AppCompatActivity
     //Adds data to Firebase database.
     public void helloWorld(){
         myRef.setValue("Hello, World!");
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
--- app/src/main/java/haitsu/groupup/SignInActivity.java
+++ app/src/main/java/haitsu/groupup/SignInActivity.java
@@ -1,12 +1,13 @@
 package haitsu.groupup;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -14,12 +15,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity implements
