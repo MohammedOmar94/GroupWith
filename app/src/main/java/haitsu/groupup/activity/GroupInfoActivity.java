572e8f96d28c068433d5494ef68d4b7e68501f30
Heights94 <mohammed_omar94@hotmail.co.uk>
2017-08-18 18:31:43 +0100

    Renamed the layout activity_group_description

--- app/src/main/java/haitsu/groupup/activity/GroupInfoActivity.java
+++ app/src/main/java/haitsu/groupup/activity/GroupInfoActivity.java
@@ -17,7 +17,7 @@ public class GroupInfoActivity extends AppCompatActivity  implements GroupInfoFr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_description);
+        setContentView(R.layout.activity_group_info);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
