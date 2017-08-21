ba95d69770181417f79fc29f7cae20e27073be0e
Heights94 <mohammed_omar94@hotmail.co.uk>
2017-08-21 14:22:57 +0100

    tabbedGroupsActivity now uses tabs which are easier to maintain.

---
+++ app/src/main/java/haitsu/groupup/ViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package haitsu.groupup;
+
+/**
+ * Created by moham on 21/08/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import haitsu.groupup.fragment.ChatsFragment;
+import haitsu.groupup.fragment.MyGroupsFragment;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position ==0) {
+            return new ChatsFragment();
+        } else if (position == 1) {
+            return new MyGroupsFragment();
+        } else return new ChatsFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
--- app/src/main/java/haitsu/groupup/activity/tabbedGroupsActivity.java
+++ app/src/main/java/haitsu/groupup/activity/tabbedGroupsActivity.java
@@ -1,6 +1,7 @@
 package haitsu.groupup.activity;
 
 import android.net.Uri;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.ActionBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,6 +23,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import haitsu.groupup.R;
+import haitsu.groupup.ViewPagerAdapter;
 import haitsu.groupup.fragment.ChatsFragment;
 import haitsu.groupup.fragment.MyGroupsFragment;
 import haitsu.groupup.fragment.NotificationsFragment;
@@ -38,7 +40,10 @@ public class tabbedGroupsActivity extends AppCompatActivity implements
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private String[] pageTitle = {"Chats", "My Groups"};
+
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -49,76 +54,43 @@ public class tabbedGroupsActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed_groups);
-        final ActionBar actionBar = getSupportActionBar();
-        // Specify that tabs should be displayed in the action bar.
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.setTitle("Group Up");
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Group Up");
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        //setting Tab layout (number of Tabs = number of ViewPager pages)
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        for (int i = 0; i < 2; i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(pageTitle[i]));
+        }
 
+        //set gravity for tab bar
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);  //set viewpager adapter
+        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
 
+        //change Tab selection when swipe ViewPager
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        // Create a tab listener that is called when the user changes tabs.
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+        //change ViewPager page when tab selected
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // When the tab is selected, switch to the
-                // corresponding page in the ViewPager.
-                mViewPager.setCurrentItem(tab.getPosition());
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            public void onTabUnselected(TabLayout.Tab tab) {
 
             }
 
             @Override
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+            public void onTabReselected(TabLayout.Tab tab) {
 
-            }
-        };
-
-        /**
-         * on swiping the viewpager make respective tab selected
-         * */
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageSelected(int position) {
-                // on changing the page
-                // make respected tab selected
-                actionBar.setSelectedNavigationItem(position);
-            }
-
-            @Override
-            public void onPageScrolled(int arg0, float arg1, int arg2) {
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int arg0) {
             }
         });
-
-        // Add 3 tabs, specifying the tab's text and TabListener
-   /*     for (int i = 0; i < 3; i++) {
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText("Tab " + (i + 1))
-                            .setTabListener(tabListener));
-        }*/
-
-        actionBar.addTab(actionBar.newTab().setText("Chats").setTabListener(tabListener));
-        actionBar.addTab(actionBar.newTab().setText("My Groups").setTabListener(tabListener));
-      //  actionBar.addTab(actionBar.newTab().setText("Joined Groups").setTabListener(tabListener));
-
-
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -147,87 +119,4 @@ public class tabbedGroupsActivity extends AppCompatActivity implements
 
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static Fragment newInstance(int sectionNumber) {
-            Fragment fragment = new PlaceholderFragment();
-            if(sectionNumber == 0){
-                fragment = new ChatsFragment();
-            }
-            else if(sectionNumber == 1) {
-                fragment = new MyGroupsFragment();
-            }
-             //fragment2 = new MyGroupsFragment();
-           // Bundle args = new Bundle();
-          //  args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-          //  fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_tabbed_groups, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            if(position==0){
-                return new ChatsFragment();
-            } else if(position==1){
-                return new MyGroupsFragment();
-            }
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
 }
