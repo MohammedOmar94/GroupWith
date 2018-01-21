34d87270bd44406ecab1837953919f89cf73b547
Heights94 <mohammed_omar94@hotmail.co.uk>
2018-01-21 19:31:53 +0000

    Uses cached data when offline

--- app/src/main/java/haitsu/groupup/activity/SplashActivity.java
+++ app/src/main/java/haitsu/groupup/activity/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 
 /**
  * Created by moham on 13/01/2018.
@@ -15,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
