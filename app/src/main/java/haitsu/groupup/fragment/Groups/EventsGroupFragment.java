f987740375a1a2f4c0c126f31d4afc8540673777
Heights94 <mohammed_omar94@hotmail.co.uk>
2018-01-27 23:34:24 +0000

    EventsFragment uses location to search for groups within 15 miles radius

--- app/src/main/java/haitsu/groupup/fragment/Groups/EventsGroupFragment.java
+++ app/src/main/java/haitsu/groupup/fragment/Groups/EventsGroupFragment.java
@@ -3,9 +3,13 @@ package haitsu.groupup.fragment.Groups;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +18,21 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryDataEventListener;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,9 +45,12 @@ import java.util.Collections;
 
 import haitsu.groupup.R;
 import haitsu.groupup.activity.Groups.GroupInfoActivity;
+import haitsu.groupup.activity.Search.ResultsActivity;
 import haitsu.groupup.other.Adapters.GroupsAdapter;
 import haitsu.groupup.other.Adapters.NotificationAdapter;
+import haitsu.groupup.other.Adapters.ResultsAdapter;
 import haitsu.groupup.other.DBConnections;
+import haitsu.groupup.other.LocationManager;
 import haitsu.groupup.other.Models.Group;
 import haitsu.groupup.other.Models.Notification;
 
@@ -41,7 +62,7 @@ import haitsu.groupup.other.Models.Notification;
  * Use the {@link EventsGroupFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EventsGroupFragment extends Fragment {
+public class EventsGroupFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -65,10 +86,25 @@ public class EventsGroupFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     private DBConnections dbConnections = new DBConnections();
+    private FirebaseUser mFirebaseUser;
+    private FirebaseAuth mFirebaseAuth;
     private DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
-    private Query groupsFromCategory;
+    private DatabaseReference eventsByLocation;
+
+    private GeoQuery geoQuery;
+
+    private GoogleApiClient mGoogleApiClient;
+    private final static int PLAY_SERVICES_REQUEST = 1000;
+    private final static int REQUEST_CHECK_SETTINGS = 2000;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
+    private LocationCallback mLocationCallback;
 
     FirebaseListAdapter<Group> groupAdapter;
+    private LocationManager locationManager = new LocationManager();
     private GroupsAdapter adapter;
 
     public EventsGroupFragment() {
@@ -115,40 +151,140 @@ public class EventsGroupFragment extends Fragment {
         Bundle extras = getActivity().getIntent().getExtras();
         groupCategory = extras.getString("GROUP_CATEGORY");
 
-        groupsFromCategory = databaseRef.child("group").child(groupCategory).orderByChild("type").equalTo("Events");
-        groupsFromCategory.keepSynced(true);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
 
-        groupsFromCategory.addValueEventListener(new ValueEventListener() {
+
+        mLocationCallback = new LocationCallback() {
             @Override
-            public void onDataChange(final DataSnapshot dataSnapshot) {
-                final ArrayList<Group> planetList = new ArrayList<Group>();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Group group = snapshot.getValue(Group.class);
-                    group.setGroupId(snapshot.getKey());
-                    group.setCategory(groupCategory);
-                    planetList.add(group);
-                    System.out.println("group " + group.getName());
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    locationManager.storeLocationData(location);
+                    getSearchResults();
                 }
-                // Orders groups in descending order according to when they were created.
-                Collections.reverse(planetList);
-                adapter = new GroupsAdapter(getContext(), planetList);
-                mListView.setAdapter(adapter);
-            };
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
             }
-        });
+        };
+
+        locationManager.setmLocationCallback(mLocationCallback);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .enableAutoManage((FragmentActivity) getActivity() /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+
+        // Handles all location logic
+        locationManager.setmFusedLocationClient(mFusedLocationClient);
 
         return view;
+    }
+
+    public void getSearchResults() {
+        eventsByLocation = databaseRef.child("group").child(groupCategory);
+        eventsByLocation.keepSynced(true);
+        GeoFire geoFire = new GeoFire(eventsByLocation);
+        // creates a new query around [37.7832, -122.4056] with a radius of 0.6 kilometers
+        // Will be done via the users current location, and the radius they selected.
+        geoQuery = geoFire.queryAtLocation(new GeoLocation(locationManager.getLatitude(), locationManager.getLongitude()), 24);
+
+
+        final ArrayList<Group> planetList = new ArrayList<Group>();
+
+
+        geoQuery.addGeoQueryDataEventListener(new GeoQueryDataEventListener() {
+
+            GeoFire.CompletionListener abc = new GeoFire.CompletionListener() {
+                @Override
+                public void onComplete(String key, DatabaseError error) {
+                    if (error != null) {
+                        System.err.println("There was an error saving the location to GeoFire: " + error);
+                    } else {
+                        System.out.println("Location saved on server successfully!");
+                    }
+                }
+            };
+
+            // Once they've done that on the groups tree
+            @Override
+            public void onDataEntered(DataSnapshot dataSnapshot, GeoLocation location) {
+                Group group = dataSnapshot.getValue(Group.class);
+                group.setGroupId(dataSnapshot.getKey());
+                group.setCategory(groupCategory);
+                System.out.println("Hey We IN " + String.format("The Key %s entered the search area at [%f,%f]", group.getName(), location.latitude, location.longitude));
+                System.out.println("hey " + dataSnapshot.getRef());
+                if ((group.getType()).equals("Events")) {
+                    // Create ArrayAdapter using the planet list.
+                    planetList.add(group);
+
+                    adapter = new GroupsAdapter(getContext(), planetList);
+                    mListView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onDataExited(DataSnapshot dataSnapshot) {
+                System.out.println("Hey Nothing to see here buddy");
+            }
+
+            @Override
+            public void onDataMoved(DataSnapshot dataSnapshot, GeoLocation location) {
+
+                System.out.println("Hey just moved within range");
+            }
+
+            @Override
+            public void onDataChanged(DataSnapshot dataSnapshot, GeoLocation location) {
+
+                System.out.println("Hey data has changed");
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
+                System.out.println("Hey ERROR");
+            }
+        });
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        locationManager.initialiseLocationRequest(getActivity());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // remove all event listeners to stop updating in the background
+        if(geoQuery != null) {
+            this.geoQuery.removeAllListeners();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // remove all event listeners to stop updating in the background
+        if(geoQuery != null) {
+            this.geoQuery.removeAllListeners();
         }
     }
 
@@ -168,6 +304,21 @@ public class EventsGroupFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
 
